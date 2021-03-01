d75486462bd93eebc6bdab7bfdcd2c0449a135d8
Princeton Poh <princetonpoh@localhost.localdomain>
2021-03-01 14:58:13 +0800

    fixing issue where git push dont appear on github

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# esc-project
+# Backend node module
+Welcome to the backend :) How do you run our program?
+1. go to the backendPrinceton branch
+2. type 'npm install'
+3. type node server.js
+4. go to the file 'blueprint' and insert that json file into postman
+5. test the apis on postman :)
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const db = require('./db/escData');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+app.get("/users/getAllUsers", async (req, res) => {
+    const users = await db.getAllUsers();
+    res.status(200).json({users})
+});
+
+// note to deal with case where users try to sign up with the same id
+app.post("/users/createUser", async (req, res) =>{
+    const result = await db.createUser(req.body);
+    res.status(200).json({id: result[0]});
+});
+
+app.delete("/users/deleteUser/:id", async (req, res) => {
+    // const result = await db.getAllUsers(req.params.id);
+    await db.getAllUsers(req.params.id);
+    res.status(200).json({success:true})
+});
+
+
+
+
+app.listen(1337, ()=> console.log('sever is running on port 1337'));
+
+//https://www.youtube.com/watch?v=cr3pX6fSUpc
