50356fe7e5dd5649e899c050effd5c0a0acdd259
We-here <72427133+We-here@users.noreply.github.com>
2021-03-02 19:47:57 +0800

    Create, get, delete, update posts

--- db/escData.js
+++ db/escData.js
@@ -19,10 +19,34 @@ function updateUser(id, user){
 };
 
 
+function getAllPosts(){
+    return knex("Users").select("*");
+}
+
+function createPost(post){
+    return knex("Posts").insert(post);
+};
+
+function deletePost(post_id){
+    return knex("Posts").where("post_id", post_id).del();
+};
+
+function updateUser(post_id, post){
+    return knex("Posts").where("post_id", post_id).update(post);
+};
+
+
+
 
 module.exports = {
     createUser,
     getAllUsers,
     deleteUser,
-    updateUser
+    updateUser,
+
+    createPost,
+    getAllPosts,
+    deletePost,
+    updatePost
+
 }
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post("/users/createUser", async (req, res) =>{
 
 app.delete("/users/deleteUser/:id", async (req, res) => {
     // const result = await db.getAllUsers(req.params.id);
-    await db.getAllUsers(req.params.id);
+    await db.deleteUser(req.params.id);
     res.status(200).json({success:true})
 });
 
@@ -30,6 +30,33 @@ app.get("/", async (req, res) => {
 
 app.put("/users/updateUser/:id",async(req,res) =>{
     await db.updateUser(req.params.id);
+    res.status(200).json({success:true})
+});
+
+
+app.get("/users/getAllPosts", async (req, res) => {
+    const posts = await db.getAllPosts();
+    res.status(200).json({posts})
+});
+
+// note to deal with case where users try to sign up with the same id
+app.post("/users/createPost", async (req, res) =>{
+    const result = await db.createPost(req.body);
+    res.status(200).json({id: result[0]});
+});
+
+app.delete("/users/deletePost/:post_id", async (req, res) => {
+    // const result = await db.getAllUsers(req.params.id);
+    await db.deletePost(req.params.id);
+    res.status(200).json({success:true})
+});
+
+app.get("/", async (req, res) => {
+    res.status(200).json({success:true})
+});
+
+app.put("/users/updatePost/:id",async(req,res) =>{
+    await db.updatePost(req.params.id);
     res.status(200).json({success:true})
 });
 
