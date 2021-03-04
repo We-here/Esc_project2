03e48d09cb4c7f0ebf7dde4e61c0e38363482c05
Jukocross <jukocross@gmail.com>
2021-03-04 10:01:54 +0800

    Added bootstrap

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,55 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Location from '../location.png';
+import Calendar from '../calendar.png';
 
-export default class Home extends Component {
-    render(){
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        const post = [];
+        var today = new Date(),
+        date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ' ' +  today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+        for (let i = 0; i < 1200; i++) {
+            post.push({
+                title: "post " + i,
+                description: "Random"
+            });
+        }
+
+        this.state = { post, currentDate: date};
+    }
+
+    render() {
         return (
-            <h1>Home Page</h1>
+            <Container style={{ width: "100%" }}>
+                <Row>
+                    <h1>Home Page</h1>
+                </Row>
+                <Row className="overflow-auto">
+                    {this.state.post.map((posts, index) => (
+                        <Card className="col-3 rounded" style={{ marginBottom: "30px", marginRight: "80px" }}>
+                            <Card.Body>
+                                <Card.Title>{posts.title}</Card.Title>
+                                <Card.Text>
+                                    <Row>
+                                        <Image width={30} height={30} className="mr-3" src={Location} alt="location" />
+                                        {posts.description}
+                                    </Row>
+                                    <Row>
+                                        <Image width={30} height={30} className="mr-3" src={Calendar} alt="location" />
+                                        {this.state.currentDate}
+                                    </Row>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    ))}
+                </Row>
+            </Container>
         );
     }
 }
+export default Home;
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,8 +1,11 @@
- import React, { Component } from 'react';
- import {Link, useRouteMatch} from "react-router-dom";
- import '../style/Signup.css'
-
- class Signup extends Component {
+import React, { Component } from 'react';
+import { Link, useRouteMatch } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+class Signup extends Component {
 
     handleSubmit = e => {
         e.preventDefault(); //Disable refresh/relaod
@@ -15,40 +18,57 @@
         };
         console.log(data);
         //Implement axios
-        //this.props.history.push('/');//Force push
+        this.props.history.push('/');//Force push
     }
 
-    render(){
+    render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <h3>Sign up</h3>
-                <div className="form-group">
-                    <label>First Name</label>
-                    <input type="text" className="form-control" placeholder="First Name" 
-                        onChange={e => this.firstName = e.target.value}/>
-                </div>
-                <div className="form-group">
-                    <label>Last Name</label>
-                    <input type="text" className="form-control" placeholder="Last Name" 
-                        onChange={e => this.lastName = e.target.value}/>
-                </div>
-                <div className="form-group">
-                    <label>Email</label>
-                    <input type="text" className="form-control" placeholder="Email" 
-                    onChange={e => this.email = e.target.value}/>
-                </div>
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Password" 
-                    onChange={e => this.password = e.target.value}/>
-                </div>
-                <div className="form-group">
-                    <label>Confirm Password</label>
-                    <input type="password" className="form-control" placeholder="Confirm Password" 
-                    onChange={e => this.confirmPassword = e.target.value}/>
-                </div>
-                <button className="btn btn-primary btn-block">Sign up</button>
-            </form>
+            <Container style={{width: "30%", padding: "30px 0px"}}>
+                <Form onSubmit={this.handleSubmit}>
+                    <h3>Sign up</h3>
+                    <Form.Group controlId="formPlaintextEmail"  className="text-left">
+                        <Form.Label style={{padding:"0px 15px"}}>
+                            First Name
+                        </Form.Label>
+                        <Col>
+                            <Form.Control type="firstName" placeholder="First Name" onChange={e => this.firstName = e.target.value} />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="formGroupLastName" className="text-left">
+                        <Form.Label style={{padding:"0px 15px"}}>
+                            Last Name
+                        </Form.Label>
+                        <Col>
+                            <Form.Control type="lastName" placeholder="Last Name" onChange={e => this.lastName = e.target.value} />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="formGroupEmail" className="text-left">
+                        <Form.Label style={{padding:"0px 15px"}}>
+                            Email
+                        </Form.Label>
+                        <Col>
+                            <Form.Control type="email" placeholder="Email" onChange={e => this.email = e.target.value} />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="formGroupPassword" className="text-left">
+                        <Form.Label style={{padding:"0px 15px"}}>
+                            Password
+                        </Form.Label>
+                        <Col>
+                            <Form.Control type="password" placeholder="Password" onChange={e => this.password = e.target.value} />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="formGroupConfirmPassword" className="text-left">
+                        <Form.Label style={{padding:"0px 15px"}}>
+                            Confirm Password
+                        </Form.Label>
+                        <Col>
+                            <Form.Control type="confirmPassword" placeholder="Confirm Password" onChange={e => this.confirmPassword = e.target.value} />
+                        </Col>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Sign up</Button>
+                </Form>
+            </Container>
         );
     }
 }
